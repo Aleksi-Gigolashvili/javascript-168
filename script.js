1a2c3a855dafde9f224b075ef4620467c22f46d0
Aleksi-Gigolashvili <aleqsigigolashvili@gmail.com>
2024-04-10 21:04:23 +0400

    add hw

---
+++ script.js
@@ -0,0 +1,30 @@
+let number = 100
+
+if ( number < 50) {
+    console.log ("less than 50");
+
+} else if (number > 20) {
+    console.log (" more than 20");
+
+} else {
+    console.log ("error");
+}
+
+let userName = "mariam"
+if (username === "mariam" ) {
+    cosole.log(true);
+}
+
+let result = userName === "mariam" ? "true" : False;
+
+console.log (nameresult)
+
+
+const Name = "mariam";
+switch (userName) {
+    case "mariam" :
+    console.log ("true");
+    break;
+default:
+    console.log ("false")
+}
